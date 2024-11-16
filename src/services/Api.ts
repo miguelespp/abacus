aa87f99cc8f0032e065d95428cb5138324f2970c
miguelespp <carlos.espinoza24@unmsm.edu.pe>
2024-11-16 03:40:52 -0500

    feat: add request with the api token

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -9,4 +9,19 @@ const Api = axios.create({
   },
 });
 
+// Add a request interceptor to add token
+
+Api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export default Api;
